e54e527a1130cc243293aaff86c456c3ce02917d
Özgür Gümüslü <thek1d@localhost.localdomain>
2020-10-19 12:28:51 +0200

    Finished datastructure for dataset and added file to ignore file

--- main.py
+++ main.py
@@ -4,12 +4,22 @@ Created on Oct 17, 2020
 @author: thek1d
 '''
 
-from Dataset.ReadFile import FileHandler
+'''
+print(ds['Scan1'])                           -> prints whole DataSet Scan1
+print(ds['Scan1'][0])                        -> prints first entry of Scan1 (Angle & Distance)
+print(ds['Scan3'][1][value['Distance']])     -> prints Distance from the second entry of Scan3
+print(ds['Scan2'][3][value['Angle']])        -> prints Angle from the fourth entry of Scan2
+'''
+
+from Dataset.dataset import FileHandler, Transformer
 
 if __name__ == '__main__':
+    
+    value = { 'Angle'    : 0,
+              'Distance' : 1}
     
     fh = FileHandler()
     ds = fh.getDataSet()
     
-    print(ds)
+    
     
