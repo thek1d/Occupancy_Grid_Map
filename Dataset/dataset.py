2f424d397d5f3d49017ae7c6f8aa7677e8c82fad
Özgür Gümüslü <thek1d@localhost.localdomain>
2020-10-20 09:52:04 +0200

    Added transformation and plotter

--- Dataset/dataset.py
+++ Dataset/dataset.py
@@ -3,17 +3,19 @@ Created on Oct 17, 2020
 
 @author: thek1d
 '''
+import math
+from   tkinter           import filedialog
+import pandas            as     pd
+import platform          as     pt
+import matplotlib.pyplot as     plt
 
-from tkinter import filedialog
-import pandas as pd
-import platform as pt
 
 class FileHandler():
     
     def __init__(self):
         self._filename = ""
-        self._dataSet = {}                              #rowwise data as dictionary -> key : Scanx
-        self._dataSetDict = {}                          #dictionary which includes reference to data dictionary
+        self._dataSet = {}                              #rowwise data as dictionary -> key : index of data
+        self._dataSetDict = {}                          #dictionary which includes reference to data dictionary (key : Name of Scan)
         
         self.__getFileName()
     
@@ -33,32 +35,69 @@ class FileHandler():
                                                          title="Open File",
                                                          filetypes=(("Excel Files", ".xlsx .xls"),("All Files", "*.*")) )
 
-    '''Generating dataframe from Excel sheet'''                                
+                                    
     def generateDataSet( self ):
         sheet_names = pd.ExcelFile(self._filename).sheet_names
-        
+                
         for num_sheet in range(len(sheet_names)):
             sheet = pd.read_excel(self._filename, sheet_name=sheet_names[num_sheet], header=None)
             for row in range(len(sheet)):
                 ''' making dictionary for Scan's {n-th entry : [Angle, Distance]} '''
                 self._dataSet.update({row : [ sheet.iloc[row][0], sheet.iloc[row][1] ]})
-            ''' linking Scan's to entries -> dataSetDict = { Scan1 : {0 : [a0, d0]}, {1 : [a1, d1]}, ... }'''    
-            self._dataSetDict.update({sheet_names[num_sheet] : self._dataSet})
-    
-    @property
-    def getDataSet(self):
+                
+            temp = self._dataSet.copy()
+            ''' linking Scan's to entries -> dataSetDict = { Scan1 : {0 : [a0, d0]}, {1 : [a1, d1]}, ... } '''    
+            self._dataSetDict.update({sheet_names[num_sheet] : temp})
+            
         return self._dataSetDict
             
 class Transformer():
     
     def __init__(self):
+        self._x = 0
+        self._y = 0
+        self._dataSetRobotCoordinates = {}
+        self._dataSetWorldCoordinates = {}
+    
+    def calculateRobotCoordinates(self, scan):
+        value = {'Angle' : 0, 'Distance' : 1}
+        for row in range(len(scan)):
+            self._x = scan[row][value['Distance']] * math.cos(math.radians(scan[row][value['Angle']]))
+            self._y = scan[row][value['Distance']] * math.sin(math.radians(scan[row][value['Angle']]))
+            self._dataSetRobotCoordinates.update({row : [self._x, self._y]})
+            
+        temp = self._dataSetRobotCoordinates.copy()
+        return temp
+    
+    def calculateWorldCoordinates(self, rcScan, posOrigToRobScan):
+        value = {'x' : 0, 'y' : 1, 'Angle' : 2}
+        for row in range(len(rcScan)):
+            
+            self._x =( posOrigToRobScan['x'] + 
+            math.cos(posOrigToRobScan['Angle']) * rcScan[row][value['x']] - 
+            math.sin(posOrigToRobScan['Angle']) * rcScan[row][value['y']] )
+            
+            self._y =( posOrigToRobScan['y'] + 
+            math.sin(posOrigToRobScan['Angle']) * rcScan[row][value['x']] + 
+            math.cos(posOrigToRobScan['Angle']) * rcScan[row][value['y']] )
+            
+            self._dataSetWorldCoordinates.update({row : [self._x, self._y]})
+        
+        temp = self._dataSetWorldCoordinates.copy()    
+        return temp  
+        
+class Plotter():
+    
+    def __init__(self):
         pass
     
-            
+    def plot(self, wcScan1, wcScan2, wcScan3):
+        value = {'x' : 0, 'y' : 1}
+        for row in range(len(wcScan1)):
+            plt.plot(wcScan1[row][value['x']], wcScan1[row][value['y']], 'ro',
+                     wcScan2[row][value['x']], wcScan2[row][value['y']], 'bo',
+                     wcScan3[row][value['x']], wcScan3[row][value['y']], 'go')
+        plt.grid(True)
+        plt.show()
         
-            
-    
-    
-
-
-        
+                    
--- main.py
+++ main.py
@@ -11,14 +11,36 @@ print(ds['Scan3'][1][value['Distance']])     -> prints Distance from the second 
 print(ds['Scan2'][3][value['Angle']])        -> prints Angle from the fourth entry of Scan2
 '''
 
-from Dataset.dataset import FileHandler, Transformer
+from Dataset.dataset import FileHandler, Transformer, Plotter
+import math
 
 if __name__ == '__main__':
     
-    value = {'Angle' : 0, 'Distance' : 1}
-    
     fh = FileHandler()
-    fh.generateDataSet()
-    ds = fh.getDataSet
+    ''' look here! '''
+    ds = fh.generateDataSet()
+    
+    tf = Transformer()
+    ''' Robot coordinates '''
+    rcScan1 = tf.calculateRobotCoordinates(ds['Scan1'])
+    rcScan2 = tf.calculateRobotCoordinates(ds['Scan2'])
+    rcScan3 = tf.calculateRobotCoordinates(ds['Scan3'])
+    
+    ''' Origin coordinates, shift vector '''
+    cordinatesOrigin2RobotScan1 = {'x' : 10, 'y' : 30, 'Angle' : math.radians(45)}
+    cordinatesOrigin2RobotScan2 = {'x' : 50, 'y' : 50, 'Angle' : math.radians(120)}
+    cordinatesOrigin2RobotScan3 = {'x' : 90, 'y' : 40, 'Angle' : math.radians(-17)}
+    
+    ''' World coordinates  '''
+    wcScan1 = tf.calculateWorldCoordinates(rcScan1, cordinatesOrigin2RobotScan1)
+    wcScan2 = tf.calculateWorldCoordinates(rcScan2, cordinatesOrigin2RobotScan2)
+    wcScan3 = tf.calculateWorldCoordinates(rcScan3, cordinatesOrigin2RobotScan3)
+            
+    pt = Plotter()
+    pt.plot(wcScan1, wcScan2, wcScan3)
+    
+
+    
+        
     
     
